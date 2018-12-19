aac85c3cfccdd5cda53110aa689e73ab1b1a0258
BT_1N3_32@BKTECH.LOCAL <BT_1N3_32@BKTECH.LOCAL>
2018-12-19 11:45:04 -0500

    Almost

---
+++ src/com/company/Position.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Position {
+    private int r;
+    private int c;
+    public Position(int r, int c)
+    {
+        this.r = r;
+        this.c = c;
+    }
+}
---
+++ src/com/company/Successors.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Successors {
+    public static Position findPosition(int num,int[][] intArr)
+    {
+        {
+            for (int i = 0; i < intArr.length;i++)
+            {
+                for(int j = 0;j< intArr[i].length;j++)
+                {
+                    return new Position(i,j);
+                }
+            }
+            return null;
+        }
+    }
+
+    public static Position[][] getSuccessorArray(int[][] intArr)
+    {
+        int plusOne;
+        {
+            for (int i = 0; i < intArr.length;i++)
+            {
+                for(int j = 0; j < intArr[i].length;j++)
+                {
+                    if(intArr[i][j] == plusOne.stream().map(v -> v + 1).collectors)
+                }
+            }
+        }
+    }
+}
